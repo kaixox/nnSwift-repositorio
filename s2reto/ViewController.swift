9ca8dff58525bfffe0dc049dde982810f982d426
Nagore <nagore@macbook-pro-de-informatica.local>
2016-07-21 02:30:46 +0200

    v2

--- s2reto/ViewController.swift
+++ s2reto/ViewController.swift
@@ -1,27 +1,7 @@
 //
 //  ViewController.swift
 //  s2reto
-//
-//  Created by Nagore on 21/7/16.
-//  Copyright © 2016 Nagore. All rights reserved.
-//
 
-/*import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}*/
 
 for i in 0...100{
     if i % 5 == 0{
