43872d11905eca262ab9988bf3e25b46a9b196d3
Nagore <nagore@macbook-pro-de-informatica.local>
2016-07-21 02:11:25 +0200

    Initial Commit

---
+++ s2retoTests/s2retoTests.swift
@@ -0,0 +1,36 @@
+//
+//  s2retoTests.swift
+//  s2retoTests
+//
+//  Created by Nagore on 21/7/16.
+//  Copyright © 2016 Nagore. All rights reserved.
+//
+
+import XCTest
+@testable import s2reto
+
+class s2retoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
